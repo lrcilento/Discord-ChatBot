d6c7841d48e86f2d97c8fb27fda87236dda83f92
lrcilento <lucascilento@protonmail.com>
2020-11-26 05:11:46 -0300

    v0.1

---
+++ mekgorod.py
@@ -0,0 +1,42 @@
+import logging
+import asyncio
+import os
+import discord
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from credentials import db, token
+
+client = discord.Client()
+channels = [781422176472924160]
+
+logging.basicConfig(level=logging.INFO)
+
+chatbot = ChatBot(
+    "Mekgorod",
+    storage_adapter={
+        'import_path': "chatterbot.storage.SQLStorageAdapter",
+        'database_uri': db
+    },
+    filters=["filters.get_recent_repeated_responses"]
+)
+
+@client.event
+async def on_ready():
+    print('Mekgorod Summoned!')    
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        if message.channel.id in channels or 0 in channels:
+            await message.channel.send(chatbot.get_response(message.content))
+        elif message.content == "mekgorod" or message.content == "Mekgorod":
+            await message.channel.send("Quem ousa?")
+            channels.append(0)
+            await asyncio.sleep(60)
+            channels.pop()
+            await message.channel.send("Não me pertube mais.")
+
+        else:
+            chatbot.get_response(message.content)
+
+client.run(token)
